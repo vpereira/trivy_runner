1b5a7c38be98c5e836127bd6555e11d89a3c820e
Victor Pereira <vpereira@suse.com>
2023-11-25 11:49:15 +0100

    Improve the orchestration using docker

--- cmd/pullworker/main.go
+++ cmd/pullworker/main.go
@@ -1,52 +1,53 @@
 package main
 
 import (
-    "context"
-    "log"
-    "os/exec"
-    "github.com/go-redis/redis/v8"
+	"context"
+	"log"
+	"os/exec"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
 var rdb *redis.Client
 
 func main() {
-    rdb = redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379", // Update with your Redis address
-        Password: "",               // No password set
-        DB:       0,                // Default DB
-    })
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379", // Update with your Redis address
+		Password: "",           // No password set
+		DB:       0,            // Default DB
+	})
 
-    // Start processing loop
-    for {
-        processQueue()
-    }
+	// Start processing loop
+	for {
+		processQueue()
+	}
 }
 
 func processQueue() {
-    // Block until an image name is available in the 'topull' queue
-    result, err := rdb.BRPopLPush(ctx, "topull", "processing", 0).Result()
-    if err != nil {
-        log.Println("Error:", err)
-        return
-    }
+	// Block until an image name is available in the 'topull' queue
+	result, err := rdb.BRPopLPush(ctx, "topull", "processing", 0).Result()
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
 
-    // Pull the Docker image
-    imageName := result
-    cmd := exec.Command("docker", "pull", imageName)
-    if err := cmd.Run(); err != nil {
-        log.Println("Failed to pull image:", imageName, "Error:", err)
-        return
-    }
+	// Pull the Docker image
+	imageName := result
+	cmd := exec.Command("docker", "pull", imageName)
+	if err := cmd.Run(); err != nil {
+		log.Println("Failed to pull image:", imageName, "Error:", err)
+		return
+	}
 
-    // Move the image name from 'processing' to 'toscan'
-    _, err = rdb.LRem(ctx, "processing", 1, imageName).Result()
-    if err != nil {
-        log.Println("Error removing image from processing queue:", err)
-        return
-    }
-    err = rdb.LPush(ctx, "toscan", imageName).Err()
-    if err != nil {
-        log.Println("Error pushing image to toscan queue:", err)
-    }
+	// Move the image name from 'processing' to 'toscan'
+	_, err = rdb.LRem(ctx, "processing", 1, imageName).Result()
+	if err != nil {
+		log.Println("Error removing image from processing queue:", err)
+		return
+	}
+	err = rdb.LPush(ctx, "toscan", imageName).Err()
+	if err != nil {
+		log.Println("Error pushing image to toscan queue:", err)
+	}
 }
--- cmd/scanworker/main.go
+++ cmd/scanworker/main.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-    "context"
-    "log"
-    "os/exec"
-    "strings"
-    "github.com/go-redis/redis/v8"
+	"context"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
 var rdb *redis.Client
 
 func main() {
-    rdb = redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379", // Update with your Redis address
-        Password: "",               // No password set
-        DB:       0,                // Default DB
-    })
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379", // Update with your Redis address
+		Password: "",           // No password set
+		DB:       0,            // Default DB
+	})
 
-    // Start processing loop
-    for {
-        processQueue()
-    }
+	// Start processing loop
+	for {
+		processQueue()
+	}
 }
 
 func processQueue() {
-    // Block until an image name is available in the 'toscan' queue
-    imageName, err := rdb.BRPop(ctx, 0, "toscan").Result()
-    if err != nil {
-        log.Println("Error:", err)
-        return
-    }
+	// Block until an image name is available in the 'toscan' queue
+	imageName, err := rdb.BRPop(ctx, 0, "toscan").Result()
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
 
-    // Sanitize the image name to create a valid filename
-    safeImageName := strings.ReplaceAll(imageName[0], "/", "_")
-    safeImageName = strings.ReplaceAll(safeImageName, ":", "_")
-    resultFileName := "results/" + safeImageName + ".json"
+	// Sanitize the image name to create a valid filename
+	safeImageName := strings.ReplaceAll(imageName[0], "/", "_")
+	safeImageName = strings.ReplaceAll(safeImageName, ":", "_")
+	resultFileName := "/app/reports/" + safeImageName + ".json"
 
-    // Run Trivy scan
-    cmd := exec.Command("trivy", "--format", "json", "--output", resultFileName, imageName[0])
-    if err := cmd.Run(); err != nil {
-        log.Println("Failed to scan image:", imageName, "Error:", err)
-        return
-    }
+	// Run Trivy scan
+	cmd := exec.Command("trivy", "--format", "json", "--output", resultFileName, imageName[0])
+	if err := cmd.Run(); err != nil {
+		log.Println("Failed to scan image:", imageName, "Error:", err)
+		return
+	}
 
-    log.Println("Scan complete for image:", imageName, "Results saved to:", resultFileName)
+	log.Println("Scan complete for image:", imageName, "Results saved to:", resultFileName)
 }
