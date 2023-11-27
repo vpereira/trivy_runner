3a9045a39fdff8c342830dbe1792531511c55445
Victor Pereira <vpereira@suse.com>
2023-11-27 13:34:54 +0100

    Use skopeo instead of docker

--- cmd/scanworker/main.go
+++ cmd/scanworker/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"os"
 	"context"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -16,22 +16,22 @@ var rdb *redis.Client
 func main() {
 
 	redisHost := os.Getenv("REDIS_HOST")
-    if redisHost == "" {
-        redisHost = "localhost" // Default value if not set
-    }
+	if redisHost == "" {
+		redisHost = "localhost" // Default value if not set
+	}
 
-    redisPort := os.Getenv("REDIS_PORT")
-    if redisPort == "" {
-        redisPort = "6379" // Default value if not set
-    }
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		redisPort = "6379" // Default value if not set
+	}
 
 	redisURL := redisHost + ":" + redisPort
 	log.Println("Connecting to Redis at:", redisURL)
 
 	rdb = redis.NewClient(&redis.Options{
 		Addr:     redisURL, // Update with your Redis address
-		Password: "",           // No password set
-		DB:       0,            // Default DB
+		Password: "",       // No password set
+		DB:       0,        // Default DB
 	})
 
 	// Start processing loop
@@ -48,7 +48,20 @@ func processQueue() {
 		return
 	}
 
-	imageName := redisAnswer[1]
+	// Split the answer
+	// [toscan registry.suse.com/bci/bci-busybox:latest|/app/images/trivy-scan-1918888852]
+	parts := strings.Split(redisAnswer[1], "|")
+	if len(parts) != 2 {
+		log.Println("Error: invalid format in Redis answer")
+		return
+	}
+
+	imageName := parts[0]
+	targetDir := parts[1]
+
+	// Delete the image when we're done
+	defer os.RemoveAll(targetDir)
+
 	// Sanitize the image name to create a valid filename
 	safeImageName := strings.ReplaceAll(imageName, "/", "_")
 	safeImageName = strings.ReplaceAll(safeImageName, ":", "_")
@@ -56,11 +69,11 @@ func processQueue() {
 
 	log.Println("Scanning image:", imageName)
 	log.Println("Saving results to:", resultFileName)
-	cmd := exec.Command("trivy", "image", "--format", "json", "--output", resultFileName, imageName)
+
+	cmd := exec.Command("trivy", "image", "--format", "json", "--output", resultFileName, "--input", targetDir)
 	if err := cmd.Run(); err != nil {
 		log.Println("Failed to scan image:", imageName, "Error:", err)
 		return
 	}
-
 	log.Println("Scan complete for image:", imageName, "Results saved to:", resultFileName)
 }
