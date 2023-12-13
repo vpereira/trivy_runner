7dff8ccbe70915c57a8ba836876356d13ad06791
Victor Pereira <vpereirabr@gmail.com>
2023-12-13 19:30:08 +0100

    Make format

--- pkg/utils/filename_test.go
+++ pkg/utils/filename_test.go
@@ -5,23 +5,22 @@ import (
 )
 
 func TestImageToFilename(t *testing.T) {
-    testCases := []struct {
-        imageName    string
-        expectedPath string
-    }{
-        {"registry.example.com/repo/image:tag", "/app/reports/registry.example.com_repo_image_tag.json"},
-        {"image:latest", "/app/reports/image_latest.json"},
-        {"image", "/app/reports/image.json"},
-        // Add more test cases as needed
-    }
+	testCases := []struct {
+		imageName    string
+		expectedPath string
+	}{
+		{"registry.example.com/repo/image:tag", "/app/reports/registry.example.com_repo_image_tag.json"},
+		{"image:latest", "/app/reports/image_latest.json"},
+		{"image", "/app/reports/image.json"},
+		// Add more test cases as needed
+	}
 
-    for _, tc := range testCases {
-        t.Run(tc.imageName, func(t *testing.T) {
-            result := ImageToFilename(tc.imageName)
-            if result != tc.expectedPath {
-                t.Errorf("ImageToFilename(%s) = %s; want %s", tc.imageName, result, tc.expectedPath)
-            }
-        })
-    }
+	for _, tc := range testCases {
+		t.Run(tc.imageName, func(t *testing.T) {
+			result := ImageToFilename(tc.imageName)
+			if result != tc.expectedPath {
+				t.Errorf("ImageToFilename(%s) = %s; want %s", tc.imageName, result, tc.expectedPath)
+			}
+		})
+	}
 }
-
