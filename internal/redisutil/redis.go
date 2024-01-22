edcf309e7d324cfb550fd951e0e7fd599a4014c6
Victor Pereira <vpereira@suse.com>
2024-01-22 12:18:06 +0100

    Add redis verbose if connection worked

--- internal/redisutil/redis.go
+++ internal/redisutil/redis.go
@@ -1,6 +1,7 @@
 package redisutil
 
 import (
+	"context"
 	"log"
 	"os"
 
@@ -15,11 +16,20 @@ func InitializeClient() *redis.Client {
 
 	log.Println("Connecting to Redis at:", redisURL)
 
-	return redis.NewClient(&redis.Options{
+	redisClient := redis.NewClient(&redis.Options{
 		Addr:     redisURL,
 		Password: "", // No password by default
 		DB:       0,  // Default DB
 	})
+
+	// Perform a ping test to check connection
+	_, err := redisClient.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis at %s: %v", redisURL, err)
+	}
+
+	log.Printf("Successfully connected to Redis at %s", redisURL)
+	return redisClient
 }
 
 // getEnv retrieves an environment variable or returns a default value.
