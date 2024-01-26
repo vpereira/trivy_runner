b3137d7fdb96669e046503fb138599715f9e96e6
Victor Pereira <vpereira@suse.com>
2024-01-26 10:14:32 +0100

    Initial code to forward errors to errbit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vpereira/trivy_runner
 go 1.21
 
 require (
+	github.com/airbrake/gobrake/v5 v5.6.1
 	github.com/alicebob/miniredis/v2 v2.31.1
 	github.com/go-redis/redismock/v9 v9.2.0
 	github.com/prometheus/client_golang v1.18.0
@@ -12,9 +13,12 @@ require (
 require (
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/caio/go-tdigest/v4 v4.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jonboulle/clockwork v0.3.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
---
+++ internal/airbrake/airbrake.go
@@ -0,0 +1,48 @@
+package airbrake
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/airbrake/gobrake/v5"
+	"github.com/vpereira/trivy_runner/internal/redisutil"
+)
+
+type AirbrakeNotifier struct {
+	Notifier *gobrake.Notifier
+	Enabled  bool
+}
+
+func NewAirbrakeNotifier() *AirbrakeNotifier {
+	projectIDStr := os.Getenv("AIRBRAKE_PROJECT_ID")
+	projectKey := os.Getenv("AIRBRAKE_PROJECT_KEY")
+	projectEnvironment := redisutil.GetEnv("AIRBRAKE_ENVIRONMENT", "development")
+
+	if projectIDStr == "" || projectKey == "" {
+		return &AirbrakeNotifier{Enabled: false}
+	}
+
+	projectID, err := strconv.ParseInt(projectIDStr, 10, 64)
+	if err != nil {
+		return &AirbrakeNotifier{Enabled: false}
+	}
+
+	airbrake := gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
+		ProjectId:   projectID,
+		ProjectKey:  projectKey,
+		Environment: projectEnvironment,
+	})
+
+	return &AirbrakeNotifier{
+		Notifier: airbrake,
+		Enabled:  true,
+	}
+}
+
+func (a *AirbrakeNotifier) NotifyAirbrake(err error) {
+	if a.Enabled && err != nil {
+		if notice := a.Notifier.Notice(err, nil, 3); notice != nil {
+			a.Notifier.SendNoticeAsync(notice)
+		}
+	}
+}
