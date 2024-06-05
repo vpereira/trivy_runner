2533915a3cc9a2dd7ed863d3c484f39eb10d9884
Victor Pereira <vpereira@suse.com>
2024-06-05 17:15:16 +0200

    Fix inline documentation

--- internal/util/main.go
+++ internal/util/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 // GetEnv retrieves an environment variable or returns a default value.
-// TODO move this to a common package
 func GetEnv(key, fallback string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
@@ -15,7 +14,6 @@ func GetEnv(key, fallback string) string {
 }
 
 // GetEnvAsInt gets an environment variable as an integer, with a fallback default value.
-// TODO move this to a common package
 func GetEnvAsInt(key string, fallback int) int {
 	valueStr := os.Getenv(key)
 	if value, err := strconv.Atoi(valueStr); err == nil {
