266b4520cfd2bd33696c529823a1034989410871
Victor Pereira <vpereira@suse.com>
2024-08-01 10:20:51 +0200

    trivy: --slow is deprecated. Instead use --parallel 1

--- internal/trivy/main.go
+++ internal/trivy/main.go
@@ -17,7 +17,7 @@ func GenerateTrivyScanCmdArgs(resultFileName, target string) []string {
 	// Check if SLOW_RUN environment variable is set to "1" and add "--slow" parameter
 	slowRun := util.GetEnv("SLOW_RUN", "0")
 	if slowRun == "1" {
-		cmdArgs = append(cmdArgs, "--slow")
+		cmdArgs = append(cmdArgs, "--parallel", "1")
 	}
 
 	cmdArgs = append(cmdArgs, "--format", "json", "--output", resultFileName, "--input", target)
